9b5f8bacd74688ea624d7989808e75635cf4ddfd
shinhajun <martinhajuna@gmail.com>
2025-02-25 12:12:59 -0500

    dfd

--- src/components/WeatherDashboard.vue
+++ src/components/WeatherDashboard.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="container my-4">
     <h2 class="mb-3 text-center">StonyBrookWeather.com</h2>
-    <h5 class="mb-3 text-center">Team SkyWolves</h5>
+    <h5 class="mb-3 text-center">By Hajun Shin</h5>
 
     <!-- Google Map -->
     <GoogleMap :src="googleMapSrc" />
